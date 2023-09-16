4828ab766e7d480ba20ad2f5a7ba40339adfd2da
similoluwa <simiprech@gmail.com>
2023-09-16 10:26:13 +0100

    js language

---
+++ index.js
@@ -0,0 +1,80 @@
+// alert("hello world")
+// document.write("Hello Septa People")
+// console.log("Hi guys, its cool to learn js")
+
+// login code
+console.log("hey")
+
+var name ="silvia"
+
+var name ="femi"
+
+// comment
+
+// hhhfjj
+
+// statement
+
+// variable
+
+// var
+// let
+// const
+
+var church= "rccg";
+
+const church77 ="Deeper Life"
+
+let name1="John"
+let name2= "Matthew"
+
+console.log(name1 + " " + name2)
+
+
+
+console.log(church);
+
+// data types
+
+// 1.string
+let sentence ="i went to the market"
+
+// 2. Number
+
+let age = 20
+let newage =10
+
+console.log(age+newage)
+
+// concertenation
+
+// 4. Boolean
+
+let isFemiAwake = false
+
+// typeof
+
+console.log (typeof (age));
+
+const website="google";
+const url = "http://www." + website + "com"
+
+console.log(url)
+
+const oldName = "bayo";
+const oldAge= 15
+
+console.log("I Know" + oldName + "is" +oldAge + "years old");
+
+const newName="Rejoice" 
+const place="Akobo"
+const school="Unilorin"
+
+console.log("My name is" + newName + "," + "I live at " + place + "," +"I attended " + school)
+
+//  var declarations are globally scoped or function/locally scoped.
+
+// let declarations can be updated within its scope and block but cannot be redeclared
+
+// const declarations maintain constant value, they can only be declared within the block they were declared and cannot be updated or re-declared
+
